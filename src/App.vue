d74af32a9bdad734a217e3c2a6f98017b0ecbc5e
Abbie <abbie@philippinedev.com>
2021-09-21 09:26:19 +0800

    Login-forgot_password-signUP interface commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,66 @@
 <template>
-  <h1>{{header}}</h1>
+  <v-app>
+    <v-app-bar app color="primary" dark >
+    </v-app-bar>
+      <v-main class="text-center">
+        <Login v-if="true"/>
+        <ForgotPassword v-if="false"/>
+        <Register v-if="false"/>
+        
+        
+        <!-- <v-container align="center" justify="center">
+          <v-row align="center" justify="center" dense>
+                <v-col cols="12" sm="8" md="4" lg="4">
+                    <v-card elevation="0">
+                    <a href="http://localhost:8080/">
+                        <v-img 
+                            src="@/assets/logo.svg" 
+                            alt="logo" contain height="50"></v-img>
+                    </a>
+                    <h1>Reaction Time</h1>
+                    <v-card-text>
+                        <v-card-actions class="text--secondary">
+                            <v-btn @click="start"
+                            x-large block dark  elevation="2">Play</v-btn>
+                        </v-card-actions>
+                    </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container> -->
+        
+
+      </v-main>
+  </v-app>
 </template>
 
 <script>
+import Login from './components/Login.vue';
+import ForgotPassword from './components/ForgotPassword.vue';
+import Register from './components/Register.vue';
 
 export default {
   name: 'App',
-  data() {
-    return {
-      header: 'First vue App'
-    }
+  components: { 
+    Login,
+    ForgotPassword,
+    Register
   },
-  
-}
+  data: () => ({
+    isPlaying: false,
+    delay: null
+  }),
+  // data(){
+  //   return {
+  //   isPlaying: false,
+  //   delay: null 
+  //   }
+  // },
+  // methods: {
+  //   start(){
+  //     this.delay = 2000
+  //     this.isPlaying = true
+  //   }
+  // }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-app>
+        <v-main>
+        <v-container class="fill-height" fluid>
+            <v-row align="center" justify="center" dense>
+                <v-col cols="12" sm="8" md="4" lg="4">
+                    <v-card elevation="0">
+                    <a href="http://localhost:8080/">
+                        <v-img 
+                            src="@/assets/logo.svg" 
+                            alt="logo" contain height="200"></v-img>
+                    </a>
+                    <v-card-text>
+                        <v-form>
+                        <v-text-field 
+                            label="Enter your email" 
+                            name="email" 
+                            prepend-inner-icon="mdi-email" 
+                            type="email" outlined></v-text-field>
+                        <v-text-field 
+                            label="Enter your password" 
+                            name="password" 
+                            prepend-inner-icon="mdi-lock" 
+                            type="password" 
+                            suffix="| Forgot?" outlined></v-text-field>
+                        <v-btn 
+                            class="rounded-0" 
+                            x-large block dark  elevation="2">Login</v-btn>
+                        <v-card-actions class="text--secondary">
+                            <v-checkbox label="Remember me"></v-checkbox>
+                            <v-spacer></v-spacer>
+                            <!-- <router-link :to="{ name: 'SignUp' }">Sign Up</router-link> -->
+                            No account? <a href="#" class="pl-2" style="color: #000000">Sign Up</a>
+                        </v-card-actions>
+                        </v-form>
+                    </v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+        </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
